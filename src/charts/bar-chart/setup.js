6022e910cf072322433195289dfab5596964d5b4
RaimonMejias <alu0101390161@ull.edu.es>
2023-03-30 22:59:14 +0000

    Creado el ejemplo de bar-chart

---
+++ src/charts/bar-chart/setup.js
@@ -0,0 +1,19 @@
+const data = [
+  { year: 2010, count: 10 },
+  { year: 2011, count: 20 },
+  { year: 2012, count: 15 },
+  { year: 2013, count: 25 },
+  { year: 2014, count: 22 },
+  { year: 2015, count: 30 },
+  { year: 2016, count: 28 },
+];
+
+export const DATA = {
+  labels: data.map((row) => { return row.year; }),
+  datasets: [{
+    label: 'Acquisitions by year',
+    data: data.map((row) => { return row.count; }),
+    backgroundColor: data.map(() => `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 0.5)`),
+    borderColor: data.map(() => `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 0.5)`),
+  }],
+};
