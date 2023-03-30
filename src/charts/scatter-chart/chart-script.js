f6e48974e82567911f6ea8194d13407838079664
Eva Peso Adán <alu0101398037@ull.edu.es>
2023-03-30 21:51:09 +0000

    Scatter chart

---
+++ src/charts/scatter-chart/chart-script.js
@@ -0,0 +1,23 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Eva Peso Adán <peso.adan.05@ull.edu.es>
+ * @author Raimon José Mejías Hernández  <raimon.mejias.35@ull.edu.es>
+ * @since Mar 30 2023
+ * 
+ * @desc Displays the data as a scatter chart
+ *       
+ * @see {@link https://github.com/ULL-ESIT-PAI-2022-2023/2022-2023-pai-data-visualization}
+ */
+
+'use strict';
+
+import { CONFIG } from './config.js';
+import { Chart, registerables } from '../../../node_modules/chart.js/dist/chart.esm.js';
+Chart.register(...registerables);
+
+let CTX = document.getElementById("myChart").getContext("2d");
+let myChart = new Chart(CTX, CONFIG);
---
+++ src/charts/scatter-chart/setup.js
@@ -0,0 +1,36 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Eva Peso Adán <peso.adan.05@ull.edu.es>
+ * @author Raimon José Mejías Hernández  <raimon.mejias.35@ull.edu.es>
+ * @since Mar 30 2023
+ * 
+ * @desc Configuration for the scatter chart
+ *       
+ * @see {@link https://github.com/ULL-ESIT-PAI-2022-2023/2022-2023-pai-data-visualization}
+ */
+
+'use strict';
+
+export const DATA = {
+  datasets: [{
+    label: 'Scatter Dataset',
+    data: [{
+      x: -10,
+      y: 0
+    }, {
+      x: 0,
+      y: 10
+    }, {
+      x: 10,
+      y: 5
+    }, {
+      x: 0.5,
+      y: 5.5
+    }],
+    backgroundColor: 'rgb(255, 99, 132)'
+  }],
+};
