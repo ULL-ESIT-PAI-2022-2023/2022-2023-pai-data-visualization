b4f88251b8fd2fa0acbc6c571882d9547de89dff
Eva Peso Adán <alu0101398037@ull.edu.es>
2023-04-10 06:23:45 +0000

    Fixing typo

--- src/charts/bar-chart-example1/setup.js
+++ src/charts/bar-chart-example1/setup.js
@@ -35,7 +35,7 @@ const data = [
 export const DATA = {
   labels: data.map((row) => { return row.year; }), // Names that are going to be shown in the Y axis.
   datasets: [{ // An array of DataChart objects containing the basic information for the chart
-    //label: 'Acquisitions by year', // The name that will appear in the legend
+    // label: 'Acquisitions by year', // The name that will appear in the legend
     data: data.map((row) => { return row.count; }), // An array of Objects, Numbers or Strings
     backgroundColor: data.map(() => `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 0.5)`), 
       // An Array of Colors for each Bar in the Chart. This color represent the inside of each bar
